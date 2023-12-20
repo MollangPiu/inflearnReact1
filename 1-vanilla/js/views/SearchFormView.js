1817d1f13a3d23cb982160bde2f643a768d4cf29
MHS <hayaruby7909@gmail.com>
2023-12-20 15:11:48 +0900

    Update: 글자수에 따라서 reset 버튼 생성

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,16 +1,35 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
+
+const tag = "searchFormView";
 
 export default class SearchFormView extends View {
     constructor() {
         super(qs("#search-form-view"));
 
+
+        this.inputElement = qs("[type=text]", this.element);
         this.resetElement = qs("[type=reset]", this.element);
+
         this.showResetButton(false);
+        this.bindEvent();   //keyup 이벤트 삽입
     }
 
 
     showResetButton(visible = true) {
         this.resetElement.style.display = visible ? "block" : "none";
+    }
+
+    bindEvent() {
+        console.log(this.inputElement);
+        on(this.inputElement, "keyup", () => this.handleKeyup());
+        //console.log('test');
+    }
+
+
+    handleKeyup() {
+        console.log(tag, "handleKeyup", this.inputElement.value);
+        const {value} = this.inputElement;
+        this.showResetButton(value.length > 0);
     }
 }
